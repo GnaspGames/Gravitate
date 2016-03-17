0d98cba466c9c73378567f45ef2a66db86f827bd
Gnasp <GnaspGames@users.noreply.github.com>
2016-03-17 11:14:12 +0000

    Improve !loop and !function plugins, easier to use

--- oc-plugins/loop.js
+++ oc-plugins/loop.js
@@ -5,6 +5,7 @@ module.exports = function(args, addCommand, addSetup)
 	var name = args[0];
 	if(name)
 	{
-		addCommand("/testfor @e[type=ArmorStand,name=SYSTEM,tag=loop_" + name + "]");
+		addCommand("/testfor @e[type=ArmorStand,name=SYSTEM,tag=loop_" + name + "]", {type:"repeating",auto:true,conditional:false});
+		addCommand("searge", {type:"repeating",auto:true,conditional:true});
 	}
 }
