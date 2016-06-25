8ae5d9bd0cda7d2c06aaa3109abf6a0fea271026
Gnasp <GnaspGames@users.noreply.github.com>
2016-06-25 20:09:07 +0100

    Update to the latest version of one-command

--- oc-plugins/run_function.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(args, addCommand, addSetup)
-{
-	var name = args[0];
-	if(name)
-	{
-		addCommand("/scoreboard players tag @e[type=ArmorStand,name=SYSTEM] add func_" + name);
-	}
-}
--- oc-plugins/run_loop.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(args, addCommand, addSetup)
-{
-	var name = args[0];
-	if(name)
-	{
-		addCommand("/scoreboard players tag @e[type=ArmorStand,name=SYSTEM] add loop_" + name);
-	}
-}
